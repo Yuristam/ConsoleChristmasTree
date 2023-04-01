4216a4659260b5546a4555573e859a1706d7f6cb
Yuristam <yuristamasanakunov@gmail.com>
2023-04-01 21:34:45 +0600

    Add project files.

---
+++ WinterInConsole/Program.cs
@@ -0,0 +1,51 @@
+﻿using WinterInConsole.Christmas_Tree;
+using WinterInConsole.Snowing;
+
+while (true)
+{
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Welcome to my console app!");
+    for (int i = 0; i < Console.BufferWidth; i++)
+    {
+        Console.Write('=');
+    }
+    Console.ResetColor();
+
+    // the Main Menu of the program
+    Console.WriteLine("Main menu\r\n" +
+                      "1) Draw Christmas Tree (without decoration).\r\n" +
+                      "2) Draw Christmas Tree (with decoration).\r\n" +
+                      "3) Draw Christmas Tree (with decoration and music)\r\n" +
+                      "4) Snowing\r\n" +
+                      "5) Exit");
+    Console.Write(">");
+
+    string input = Console.ReadLine().Trim().ToLower();
+
+    switch (input)
+    {
+        case "1":
+            ChristmasTree.DrawChristmasTree();
+            break;
+        case "2":
+            ChristmasTree.DrawDecoratedChristmasTree();
+            break;
+        case "3":
+            ChristmasTree.DrawDecoratedTreeWithMusic();
+            break;
+        case "4":
+            Console.Clear();
+            Snow.Snowing();
+            break;
+        case "5":
+            Environment.Exit(0);
+            break;
+        default:
+            Console.Clear();
+            Console.WriteLine("Please, write something that is in the list above.");
+            Thread.Sleep(1000);
+            Console.Clear();
+            break;
+    }
+}
---
+++ WinterInConsole/Snowing/Snow.cs
@@ -0,0 +1,42 @@
+﻿namespace WinterInConsole.Snowing
+{
+    internal class Snow
+    {
+        public int X { get; set; }
+        public char Symbol { get; set; }
+
+        public Snow(int x, char symbol)
+        {
+            X = x;
+            Symbol = symbol;
+        }
+        public void DrawSnow()
+        {
+            for (int i = 0; i < 28; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.SetCursorPosition(X, i);
+                    Console.WriteLine(Symbol);
+                    Task.Delay(100).Wait();
+                    Console.SetCursorPosition(X, i);
+                    Console.WriteLine(' ');
+                }
+            }
+        }
+        public static void Snowing()
+        {
+            Console.CursorVisible = false;
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            while (true)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Random random = new Random();
+                    Snow snow = new(random.Next(31, 80), '*');
+                    snow.DrawSnow();
+                }
+            }
+        }
+    }
+}
